d027ab291f2c6968042999a656f15292182db1c4
FrancisFlow <iamngigikaranja1@aol.com>
2021-08-29 01:20:04 +0300

    I'm just doing my thing

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from "./heroes/hero";
+
+export const HEROES: Hero [] = [
+    {id: 1, name: "Sasuke Uchiha", strongestJutsu: "Amaterasu"},
+    {id: 2, name:"Kakashi Senpai", strongestJutsu: "Copy Ninja"},
+    {id: 3, name:"Jiraya Sense", strongestJutsu:"Ero Senin"},
+    {id: 4, name:"Gara Hokage", strongestJutsu:"Sand burial"},
+    {id: 5, name: "Orochimaru", strongestJutsu: "Reanimation"},
+    {id: 6, name: "Sakura", strongestJutsu: "Narutoooo!"},
+    {id: 6, name: "I forget", strongestJutsu:"Katsuga"},
+    {id: 7, name:"Hinata-sama", strongestJutsu:"Tiger Palms"},
+    {id: 8, name:"Neji Hyugo", strongestJutsu:"64 palm triagram"},
+    {id: 9, name:"Pain", strongestJutsu:"Chibaku Tensei"},
+    {id:10, name:"Madara Uchiha", strongestJutsu:"Reinnigan"}
+];
+// Don't forget the ending tags ; in the array
